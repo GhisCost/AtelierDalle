b4f72a837d5ac4ac16d892817bdeb1547435bb17
Ghis.cost <ghis.cost@gmail.com>
2026-02-26 14:37:56 +0100

    modif sur les pages choix portrait et portrait

--- src/Controller/PortraitHabitantController.php
+++ src/Controller/PortraitHabitantController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\PortraitHabitantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PortraitHabitantController extends AbstractController
 {
-    #[Route('/portrait/habitant', name: 'app_portrait_habitant')]
-    public function index(): Response
+    #[Route('/portrait/{id}', name: 'app_portrait_habitant')]
+    public function index(PortraitHabitantRepository $portraitHabitantRepository, int $id): Response
     {
+        $portrait=$portraitHabitantRepository->find($id);
+
+     
+
         return $this->render('portrait_habitant/index.html.twig', [
-            'controller_name' => 'PortraitHabitantController',
+           'portrait'=>$portrait,
         ]);
     }
 }
